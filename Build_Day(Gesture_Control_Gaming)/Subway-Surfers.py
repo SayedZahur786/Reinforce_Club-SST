6953ab3ee7fbd8a9c268b93076d09deec5b5199f
jatan-trivedi-sst <jatan.25bcs10137@sst.scaler.com>
2025-09-11 18:39:10 +0530

    Added Subway Surfers gesture control code by Jatan Trivedi

---
+++ Build_Day(Gesture_Control_Gaming)/Subway-Surfers.py
@@ -0,0 +1,33 @@
+import cv2
+import pyautogui
+from cvzone.HandTrackingModule import HandDetector
+detector = HandDetector(detectionCon=0.5,maxHands=2)
+cam=cv2.VideoCapture(0)
+cam.set(3,640)
+cam.set(4,480)
+while True: 
+    success ,frame =cam.read()
+    img=cv2.flip(frame,1)
+    hand,img=detector.findHands(img)
+    if hand and hand[0]['type']=="Right":
+        fingers=detector.fingersUp(hand[0])
+        totalFingers=fingers.count(1)
+        cv2.putText(img, f'Fingers: {totalFingers}', (50, 50), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
+        if totalFingers==5:
+            pyautogui.keyDown('right')
+            pyautogui.keyUp('left') 
+        if totalFingers==0:
+            pyautogui.keyDown('left')
+            pyautogui.keyUp('right')
+    if hand and hand[0]['type']=="Left":
+        fingers=detector.fingersUp(hand[0])
+        totalFingers=fingers.count(1)
+        cv2.putText(img, f'Fingers: {totalFingers}', (50, 50), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
+        if totalFingers==5:
+            pyautogui.keyDown('up')
+            pyautogui.keyUp('down') 
+        if totalFingers==0:
+            pyautogui.keyDown('down')
+            pyautogui.keyUp('up')
+    # cv2.imshow('Livefeed',img)  # Changed from 'frame' to 'img'
+    # cv2.waitKey(1)
